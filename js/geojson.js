9a2da40f39a59ff154bb088fe663a371e8b81045
Mason <masonbindl@gmail.com>
2019-02-19 22:01:17 -0800

    update geojson.js

--- js/geojson.js
+++ js/geojson.js
@@ -17,6 +17,19 @@ function createMap(){
     //call getData function
     getData(map);
 };
+//added at Example 2.3 line 20...function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
 //function to retrieve the data and place it on the map
 function getData(map){
         $.ajax("data/MegaCities.geojson", {
@@ -31,13 +44,12 @@ function getData(map){
                 opacity: 1,
                 fillOpacity: 0.8
             };
-
             //create a Leaflet GeoJSON layer and add it to the map
             L.geoJson(response, {
                 pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
+                    return L.circleMarker(latlng, geojsonMarkerOptions);},
+                onEachFeature: onEachFeature}
+            ).addTo(map);
         }
     });
 };
