1bdb2d3ef4ac9c580d4da96cc81d53dde41848a4
Mason <masonbindl@gmail.com>
2019-02-19 21:36:30 -0800

    update geojson.js

--- js/geojson.js
+++ js/geojson.js
@@ -2,8 +2,8 @@
 function createMap(){
     //create the map
     var map = L.map('map', {
-        center: [20, 0],
-        zoom: 2
+        center: [15, 110],
+        zoom: 3
     });
 
     //add base tilelayer
@@ -19,15 +19,26 @@ function createMap(){
 };
 //function to retrieve the data and place it on the map
 function getData(map){
-    //load the data
-    $.ajax("data/MegaCities.geojson", {
+        $.ajax("data/MegaCities.geojson", {
         dataType: "json",
         success: function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
 
             //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response).addTo(map);
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
         }
     });
 };
-
 $(document).ready(createMap);
