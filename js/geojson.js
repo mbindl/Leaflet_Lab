3050cf59c1f6d88cbb9ae4ebc8e2cd0040e418ca
Mason <masonbindl@gmail.com>
2019-03-02 20:36:14 -0800

    updated geojson.js

--- js/geojson.js
+++ js/geojson.js
@@ -67,6 +67,7 @@ function updatePropSymbols(map, attribute){
             popup.bindToLayer();
         };
     });
+    updateLegend(map,attribute)
 };
 //Build an attributes array from the data
 function processData(data){
@@ -86,6 +87,117 @@ function processData(data){
     return attributes;
 };
 
+// Create Legend
+//function to create the legend
+function createLegend(map, attributes){
+    var LegendControl = L.Control.extend({
+        options: {
+            position: 'bottomright'
+        },
+
+        onAdd: function (map) {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'legend-control-container');
+
+            //add temporal legend div to container
+            $(container).append('<div id="temporal-legend">')
+
+            //Step 1: start attribute legend svg string
+            var svg = '<svg id="attribute-legend" width="160px" height="100px">';
+
+            //array of circle names to base loop on
+            var circles = {
+                max: 20,
+                mean: 40,
+                min: 60
+            };
+
+            //Step 2: loop to add each circle and text to svg string
+            for (var circle in circles){
+                //circle string
+
+                svg += '<circle class="legend-circle" id="' + circle + '" fill="#E34132" fill-opacity="0.9" stroke="#000000" cx="30"/>';
+
+                //text string
+                svg += '<text id="' + circle + '-text" x="65" y="' + circles[circle] + '"></text>';
+
+            };
+
+            //close svg string
+            svg += "</svg>";
+            //add attribute legend svg to container
+            $(container).append(svg);
+
+            return container;
+        }
+    });
+
+    map.addControl(new LegendControl());
+
+    updateLegend(map, attributes[0]);
+};
+
+//Calculate the max, mean, and min values for a given attribute
+function getCircleValues(map, attribute){
+    //start with min at highest possible and max at lowest possible number
+    var min = Infinity,
+        max = -Infinity;
+
+    map.eachLayer(function(layer){
+        //get the attribute value
+        if (layer.feature){
+            var attributeValue = Number(layer.feature.properties[attribute]);
+
+            //test for min
+            if (attributeValue < min){
+                min = attributeValue;
+            };
+
+            //test for max
+            if (attributeValue > max){
+                max = attributeValue;
+            };
+        };
+    });
+
+    //set mean
+    var mean = (max + min) / 2;
+
+    //return values as an object
+    return {
+        max: max,
+        mean: mean,
+        min: min
+    };
+};
+
+//Update the legend with new attribute
+function updateLegend(map, attribute){
+    //create content for legend
+    var year = attribute.split("_")[1];
+    var content = "Average Daily Vehicle Trips " + year;
+
+    //replace legend content
+    $('#temporal-legend').html("<b>" + content + "</b>");
+
+    //get the max, mean, and min values as an object
+    var circleValues = getCircleValues(map, attribute);
+
+    for (var key in circleValues){
+      //get the radius
+      var radius = calcPropRadius(circleValues[key]);
+
+      //Step 3: assign the cy and r attributes
+      $('#'+key).attr({
+          cy: 59 - radius,
+          r: radius
+      });
+
+      //Step 4: add legend text
+      $('#'+key+'-text').text(circleValues[key].toFixed(1) + " Vehicles");
+
+    };
+};
 
 // Create new sequence controls
 function createSequenceControls(map, attributes){
@@ -237,7 +349,9 @@ function getData(map){
            //create an attributes array
             var attributes = processData(response);
             createPropSymbols(response, map, attributes);
-            createSequenceControls(map, attributes)
+            createSequenceControls(map, attributes);
+            
+            createLegend(map, attributes);
         }
     });
 };
--- js/main.js
+++ js/main.js
@@ -86,7 +86,24 @@ function processData(data){
     return attributes;
 };
 
+function createLegend(map, attributes){
+    var LegendControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
 
+        onAdd: function (map) {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'legend-control-container');
+
+            //PUT YOUR SCRIPT TO CREATE THE TEMPORAL LEGEND HERE
+
+            return container;
+        }
+    });
+
+    map.addControl(new LegendControl());
+};
 // Create new sequence controls
 function createSequenceControls(map, attributes){
     var SequenceControl = L.Control.extend({
@@ -194,8 +211,13 @@ function pointToLayer(feature, latlng, attributes){
     //create popup
     var popup = new Popup(feature.properties, attribute, layer, geojsonMarkerOptions.radius);
     
-    //add popup
-    popup.bindToLayer();
+    var popup2 = Object.create(popup);
+
+    //change the content of popup 2
+    popup2.content = "<h2>" + popup.population + " million</h2>";
+
+    //add popup to circle marker
+    popup2.bindToLayer();
     
     //event listeners to open popup on hover
     layer.on({
